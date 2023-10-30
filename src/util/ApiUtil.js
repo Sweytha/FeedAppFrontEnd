51c90ade7d0ac7decda8a6f96025d9ea7c65e8a6
sweythak <sweythak@gmail.com>
2023-10-29 20:51:32 -0400

    VerifyEmail API and component

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -39,3 +39,24 @@ export const registerApi = async (username, password, emailId, firstName, lastNa
         return response;
     }
 }
+
+
+export const verifyEmailApi = async (token) => {
+    let response = frameResponse();
+    try {
+        const url = `${API_BASE_URL}/user/verify/email`;
+        const apiResponse = await axios.get(url, {
+            headers: { Authorization: frameToken(token) },
+        });
+        if (apiResponse.status === 200) {
+            response = frameResponse(1, apiResponse.data);
+        }
+    } catch (err) {
+        if (err.response) {
+            response = frameResponse(0, err.response.data.message);
+        }
+        console.log(err);
+    } finally {
+        return response;
+    }
+};
