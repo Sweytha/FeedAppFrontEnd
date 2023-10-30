cee6749e430f7deae51b117828406f76ca150196
sweythak <sweythak@gmail.com>
2023-10-29 21:00:44 -0400

    Created login, forgotPassword and resetPassword API/components

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -60,3 +60,83 @@ export const verifyEmailApi = async (token) => {
         return response;
     }
 };
+
+
+export const loginApi = async (username, password) => {
+    let response = frameResponse();
+
+    try {
+        const url = `${API_BASE_URL}/user/login`;
+        const apiResponse = await axios.post(url, { username, password });
+        if (apiResponse.status === 200) {
+            //if the login is sucessful , get the user Data and token from the response 
+            const payLoad = {
+                userData: apiResponse.data,
+                token: apiResponse.headers.authorization,
+            };
+            response = frameResponse(1, payLoad);
+        }
+    } catch (err) {
+        if (err.response) {
+            response = frameResponse(0, err.response.data.message);
+        }
+        console.log(err);
+    } finally {
+        return response;
+    }
+};
+
+//this is calling the forgotPasswordAPI from the backend 
+export const forgotPasswordApi = async (email) => {
+    let response = frameResponse();
+
+    try {
+        //email is passed here as a path variable 
+        const url = `${API_BASE_URL}/user/reset/${email}`;
+        //use axios to create a get request and pass the url to it
+        const apiResponse = await axios.get(url);
+        //if the response is 200 OK then set the status code of frameResponse to 1
+        if (apiResponse.status === 200) {
+            response = frameResponse(1);
+        }
+    } catch (err) {
+        if (err.response) {
+            response = frameResponse(0, err.response.data.message);
+        }
+        console.log(err);
+    } finally {
+        return response;
+    }
+
+};
+
+//calls the reset password API from the backend 
+export const resetPasswordApi = async (token, password) => {
+
+    let response = frameResponse();
+    try {
+        //call the API from the backend 
+        const url = `${API_BASE_URL}/user/reset`;
+        //do a post using axios and pass the password as a request body
+        //passing the token to the header of the request
+        const apiResponse = await axios.post(
+            url,
+            {
+                password,
+            },
+            { headers: { Authorization: frameToken(token) } }
+        );
+        if (apiResponse.status === 200) {
+            response = frameResponse(1);
+        }
+    } catch (err) {
+        if (err.response) {
+            response = frameResponse(0, err.response.data.message);
+        }
+        console.log(err);
+    } finally {
+        return response;
+    }
+
+
+};
